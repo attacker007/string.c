c0c330a597ee0f0714dd17db1c4d8b8fec0052fc
sutharsan m <sutharsansukgu@gmail.com>
2026-03-12 11:07:15 +0530

    Create str-3.c

---
+++ str-3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[30];
+    scanf("%s",&str);
+    for(int i=0;str[i]!='@';i++){
+        printf("%c",str[i]);
+    }
+    return 0;
+}
